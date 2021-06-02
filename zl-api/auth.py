26b23e4fb42da7485fab40863b7c8c548330080f
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2021-06-02 17:41:53 -0300

    remove unused import

--- zl-api/auth.py
+++ zl-api/auth.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, request
 from flask_jwt_extended import create_access_token
 from .database.models import User
-from .database.db import db
 import datetime
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
