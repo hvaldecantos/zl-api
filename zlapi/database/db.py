e0a7d62769db9d817fba885a20dc2ef6f3a8969c
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2021-06-07 13:26:15 -0300

    remove hardcoded db name

--- zlapi/database/db.py
+++ zlapi/database/db.py
@@ -1,6 +1,7 @@
 from flask_mongoengine import MongoEngine
 import click
 from flask.cli import with_appcontext
+from flask import current_app
 
 
 db = MongoEngine()
@@ -9,7 +10,8 @@ db = MongoEngine()
 @with_appcontext
 def delete_db_command():
     """Delete existing db."""
-    db.connection.drop_database("zl-api")
+    db_name = current_app.config['MONGODB_DB']
+    db.connection.drop_database(db_name)
     click.echo('database deleted.')
 
 def init_app(app):
