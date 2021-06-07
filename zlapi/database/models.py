7d82bf5af06607c9fa1b6798f16b91d3ac7068cf
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2021-06-07 13:26:55 -0300

    add name and created_at fields in User model

--- zlapi/database/models.py
+++ zlapi/database/models.py
@@ -1,12 +1,19 @@
 from .db import db
 from flask_bcrypt import generate_password_hash, check_password_hash
+import datetime
+
 
 class User(db.Document):
+    name = db.StringField(required=True)
     email = db.EmailField(required=True, unique=True)
     password = db.StringField(required=True, min_length=6)
+    created_at = db.DateTimeField(default=datetime.datetime.utcnow)
 
     def hash_password(self):
         self.password = generate_password_hash(self.password).decode('utf8')
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return '<User name: %r email: %r>' % (self.name, self.email)
